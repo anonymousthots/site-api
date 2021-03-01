27846166c8c3a662d7908b1b8a785fc00bfa48e0
anonymousthots <gina@thesistesting.com>
2021-02-28 16:31:55 -0800

    /users/:username GET successful setup => returns user instance as JSON

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,23 @@
-var express = require('express');
-var router = express.Router();
+// GET("/users/:username") => returns user instance data
+
+const express = require('express');
+const router = express.Router();
+const User = require('../models/user');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/:username', async (req, res, next) => {
+  const username = req.params.username;
+
+  User.find({ username })
+    .exec(function(err, users) {
+      if (err) return next(err);
+
+      const data = users.map(({_id, email, username}) => {
+        return { _id, email, username };
+      })
+
+      res.status(200).json(data);
+    })
 });
 
 module.exports = router;
